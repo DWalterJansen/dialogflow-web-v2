3134bf48586cbb5e72e07da2cf519c615a50fd66
DWalterJansen <davidwalterjansengithub@gmail.com>
2020-10-08 11:50:55 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 export default {
     // endpoint: 'https://dialogflow-web-v2.core.ushaflow.io', // <- enter your gateway URL here
+    // EndPoint Anemia
     endpoint: 'https://anemia-umlj.core.ushaflow.io', // <- enter your gateway URL here
     muted: true, // <- mute microphone at start
     start_suggestions: [], // <- array of suggestions, displayed at the start screen
